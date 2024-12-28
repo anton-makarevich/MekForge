4e5f331bbac922bbb74a80f9d6ddccf42f87c084
Anton Makarevich <anton.makarevich@hotmail.com>
2024-12-28 23:44:36 +0100

    chore: add missing tests for player viewmodel

--- tests/MekForge.Core.Tests/ViewModels/Wrappers/PlayerViewModelTests.cs
+++ tests/MekForge.Core.Tests/ViewModels/Wrappers/PlayerViewModelTests.cs
@@ -21,6 +21,19 @@ public class PlayerViewModelTests
         // Assert
         playerViewModel.Units.First().Chassis.Should().Be(unit.Chassis);
     }
+
+    [Fact]
+    public void AddUnit_ShouldNotAddUnitToPlayer_IfSelectedUnitIsNull()
+    {
+        // Arrange
+        var playerViewModel = new PlayerViewModel(new Player(Guid.NewGuid(), "Player1"),[]);
+    
+        // Act
+        playerViewModel.AddUnitCommand.Execute(null);
+    
+        // Assert
+        playerViewModel.Units.Count.Should().Be(0);
+    }
     
     [Fact]
     public void Name_ShouldReturnPlayerName()
@@ -33,5 +46,47 @@ public class PlayerViewModelTests
     
         // Assert
         name.Should().Be("Player1");
+    }
+    
+    [Fact]
+    public void CanAddUnit_ShouldReturnTrue_IfSelectedUnitIsNotNull()
+    {
+        // Arrange
+        var playerViewModel = new PlayerViewModel(new Player(Guid.NewGuid(), "Player1"),[]);
+        var unit = MechFactoryTests.CreateDummyMechData(); // Create a new unit
+        playerViewModel.SelectedUnit = unit;
+    
+        // Act
+        var canAddUnit = playerViewModel.CanAddUnit;
+    
+        // Assert
+        canAddUnit.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void CanAddUnit_ShouldReturnFalse_IfSelectedUnitIsNull()
+    {
+        // Arrange
+        var playerViewModel = new PlayerViewModel(new Player(Guid.NewGuid(), "Player1"),[]);
+    
+        // Act
+        var canAddUnit = playerViewModel.CanAddUnit;
+    
+        // Assert
+        canAddUnit.Should().BeFalse();
+    } 
+    
+    [Fact]
+    public void AvailableUnits_ShouldHaveCorrectValue()
+    {
+        // Arrange
+        var unit = MechFactoryTests.CreateDummyMechData(); // Create a new unit
+        var playerViewModel = new PlayerViewModel(new Player(Guid.NewGuid(), "Player1"),[unit]);
+    
+        // Act
+        var availableUnits = playerViewModel.AvailableUnits.ToList();
+    
+        // Assert
+        availableUnits.Contains(unit).Should().BeTrue();
     }
 }
