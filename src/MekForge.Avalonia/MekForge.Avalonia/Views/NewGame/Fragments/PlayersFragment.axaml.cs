6f9b060ca6630e24531d2779f62ed9f8d58cd20d
Anton Makarevich <anton.makarevich@hotmail.com>
2024-12-29 21:34:10 +0100

    feat: support responsive ui for new game

---
+++ src/MekForge.Avalonia/MekForge.Avalonia/Views/NewGame/Fragments/PlayersFragment.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Sanet.MekForge.Avalonia.Views.NewGame.Fragments
+{
+    public partial class PlayersFragment : UserControl
+    {
+        public PlayersFragment()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/MekForge.Avalonia/MekForge.Avalonia/Views/NewGame/NewGameViewWide.axaml.cs
@@ -0,0 +1,10 @@
+namespace Sanet.MekForge.Avalonia.Views.NewGame
+{
+    public partial class NewGameViewWide : NewGameView
+    {
+        public NewGameViewWide()
+        {
+            InitializeComponent();
+        }
+    }
+}
