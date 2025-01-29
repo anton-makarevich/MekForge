813d8f9103591ac48efe00a84f1f5e0f7bec1a96
Anton Makarevich <anton.makarevich@gmail.com>
2025-01-29 22:04:35 +0100

    refactor: move json context to separate file

--- src/MekForge.Core/Models/Game/Commands/GameCommand.cs
+++ src/MekForge.Core/Models/Game/Commands/GameCommand.cs
@@ -1,10 +1,7 @@
 using Sanet.MekForge.Core.Services.Localization;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace Sanet.MekForge.Core.Models.Game.Commands;
 
-[JsonConverter(typeof(GameCommandJsonConverterFactory))]
 public abstract record GameCommand
 {
     public required Guid GameOriginId { get; set; }
@@ -13,13 +10,8 @@ public abstract record GameCommand
 
     public GameCommand CloneWithGameId(Guid newGameId)
     {
-        var options = new JsonSerializerOptions
-        {
-            IncludeFields = true,
-            TypeInfoResolver = GameCommandContext.Default
-        };
-        var json = JsonSerializer.Serialize(this, GetType(), options);
-        var clone = (GameCommand)JsonSerializer.Deserialize(json, GetType(), options)!;
+        var json = GameCommandTypeRegistry.Serialize(this);
+        var clone = GameCommandTypeRegistry.Deserialize(json)!;
         clone.GameOriginId = newGameId;
         return clone;
     }
