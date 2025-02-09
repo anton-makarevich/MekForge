aced2b5c00ba8d3e1c779c558478c20c4b868bd1
Anton Makarevich <anton.makarevich@gmail.com>
2025-02-09 22:02:10 +0100

    feat: handle torso rotation

--- tests/MekForge.Core.Tests/Models/Game/BaseGameTests.cs
+++ tests/MekForge.Core.Tests/Models/Game/BaseGameTests.cs
@@ -7,13 +7,15 @@ using Sanet.MekForge.Core.Models.Game.Phases;
 using Sanet.MekForge.Core.Models.Game.Transport;
 using Sanet.MekForge.Core.Models.Map;
 using Sanet.MekForge.Core.Models.Map.Terrains;
+using Sanet.MekForge.Core.Models.Units.Mechs;
+using Sanet.MekForge.Core.Tests.Data;
 using Sanet.MekForge.Core.Utils.Generators;
 using Sanet.MekForge.Core.Utils.TechRules;
 
 namespace Sanet.MekForge.Core.Tests.Models.Game;
 
 public class BaseGameTests() : BaseGame(BattleMap.GenerateMap(5, 5, new SingleTerrainGenerator(5,5, new ClearTerrain())),
-        Substitute.For<IRulesProvider>(),
+        new ClassicBattletechRulesProvider(),
         Substitute.For<ICommandPublisher>())
 {
     [Fact]
@@ -43,7 +45,98 @@ public class BaseGameTests() : BaseGame(BattleMap.GenerateMap(5, 5, new SingleTe
         TurnPhase.ShouldBe(PhaseNames.Start);
     }
 
-    // Additional tests for common functionalities can be added here
+    [Fact]
+    public void OnWeaponConfiguration_DoesNothing_WhenPlayerNotFound()
+    {
+        // Arrange
+        var command = new WeaponConfigurationCommand
+        {
+            GameOriginId = Id,
+            PlayerId = Guid.NewGuid(),
+            UnitId = Guid.NewGuid(),
+            Configuration = new WeaponConfiguration
+            {
+                Type = WeaponConfigurationType.TorsoRotation,
+                Value = (int)HexDirection.Bottom
+            }
+        };
+
+        // Act
+        OnWeaponConfiguration(command);
+
+        // Assert
+        // No exception should be thrown
+    }
+
+    [Fact]
+    public void OnWeaponConfiguration_DoesNothing_WhenUnitNotFound()
+    {
+        // Arrange
+        var joinCommand = new JoinGameCommand
+        {
+            PlayerId = Guid.NewGuid(),
+            PlayerName = "Player1",
+            GameOriginId = Guid.NewGuid(),
+            Units = [],
+            Tint = "#FF0000"
+        };
+        OnPlayerJoined(joinCommand);
+        var player = Players.First();
+        var command = new WeaponConfigurationCommand
+        {
+            GameOriginId = Id,
+            PlayerId = player.Id,
+            UnitId = Guid.NewGuid(),
+            Configuration = new WeaponConfiguration
+            {
+                Type = WeaponConfigurationType.TorsoRotation,
+                Value = (int)HexDirection.Bottom
+            }
+        };
+
+        // Act
+        OnWeaponConfiguration(command);
+
+        // Assert
+        // No exception should be thrown
+    }
+
+    [Fact]
+    public void OnWeaponConfiguration_RotatesTorso_WhenConfigurationIsTorsoRotation()
+    {
+        // Arrange
+        var joinCommand = new JoinGameCommand
+        {
+            PlayerId = Guid.NewGuid(),
+            PlayerName = "Player1",
+            GameOriginId = Guid.NewGuid(),
+            Units = [MechFactoryTests.CreateDummyMechData()],
+            Tint = "#FF0000"
+        };
+        OnPlayerJoined(joinCommand);
+        var player = Players.First();
+        var mech = player.Units.First() as Mech;
+        mech?.Deploy(new HexPosition(new HexCoordinates(3, 3), HexDirection.BottomLeft));
+
+        var command = new WeaponConfigurationCommand
+        {
+            GameOriginId = Id,
+            PlayerId = player.Id,
+            UnitId = mech!.Id,
+            Configuration = new WeaponConfiguration
+            {
+                Type = WeaponConfigurationType.TorsoRotation,
+                Value = (int)HexDirection.Bottom
+            }
+        };
+
+        // Act
+        OnWeaponConfiguration(command);
+
+        // Assert
+        mech.TorsoDirection.ShouldBe(HexDirection.Bottom);
+    }
+
     public override void HandleCommand(GameCommand command)
     {
         throw new NotImplementedException();
